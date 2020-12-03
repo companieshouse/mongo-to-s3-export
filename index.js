be2388554fc71927afa191e69d14705392273e8f
markpit <mpitten@companieshouse.gov.uk>
2020-12-03 12:48:07 +0000

    OBJ-140 Modifying index.js to make it more generic. Now gets mongo collection and db names from env props

--- index.js
+++ index.js
@@ -3,92 +3,97 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const url = require('url');
-
 const exec = require('child_process').exec;
-
-const mongoURI = process.env.MONGO_URL; //with port
-const s3Path = process.env.S3_PATH;
-const mongoURIparsed = url.parse(mongoURI);
-const host = mongoURIparsed.host;
-const dbName = mongoURIparsed.path.split('/')[1];
-
-const username = mongoURIparsed.auth ? mongoURIparsed.auth.split(":")[0] : undefined;
-const password = mongoURIparsed.auth ? mongoURIparsed.auth.split(":")[1] : undefined;
-
-const s3bucket = new AWS.S3({
-  params: {
-    Bucket: s3Path
-  }
-});
-
 
 module.exports.handler = function(event, context, cb) {
     process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
     console.log(process.env['PATH']);
-    let fileName = (new Date()).toDateString().replace(/ /g, "") +"_"+ (new Date()).getTime();
-    let folderName = '/tmp/' + fileName 
+    
+    const mongoURI = process.env.MONGO_URL; // with port
+    const mongoDBName = process.env.MONGO_DATABASE;
+    const mongoCollection = process.env.MONGO_COLLECTION;
+    const mongoURIparsed = url.parse(mongoURI);
+    const mongoHost = mongoURIparsed.host;
+    const mongoUsername = mongoURIparsed.auth ? mongoURIparsed.auth.split(':')[0] : undefined;
+    const mongoPassword = mongoURIparsed.auth ? mongoURIparsed.auth.split(':')[1] : undefined;
 
+    const s3Path = process.env.S3_PATH;
+    // split s3Path into bucket name and folder
+    const bucketName = s3Path.split('/', 1);
+    const folder = s3Path.slice(bucketName[0].length).substring(1) + '/' + mongoCollection + '/';
 
-    //split s3Path into bucket name and folder
-    var bucketName = s3Path.split("/", 1);
-    var folder = s3Path.slice(bucketName[0].length).substring(1) +'/'; 
+    console.log(`bucketName = ${bucketName}`);
+    console.log(`folder = ${folder}`);
 
-    //clean out bucket
-    s3bucket.listObjects({Bucket: bucketName[0], Delimiter: '/', Prefix: folder}, function (err, data) {
-            if (err) {
-                console.log("error listing bucket objects "+err);
-                return;
-            }
-            var items = data.Contents;
-            console.log("found: " + items.length + " objects");
+    const s3bucket = new AWS.S3({
+        params: {
+            Bucket: s3Path
+        }
+    });
 
-            for (var i = 0; i < items.length; i += 1) {
-                //console.log("delete: " + items[i].Key);
-                var deleteParams = {Bucket: bucketName[0], Key: items[i].Key};
-                //s3bucket.deleteObject(deleteParams);
-                s3bucket.deleteObject(deleteParams, function (err, data) {
-                    if (err) {
-                        console.log("delete err " + deleteParams.Key);
-                    } else {
-                        console.log("deleted " + deleteParams.Key);
-                    }
-                });
-            }
-        });
+    const now = new Date();
+    const outputFileName = mongoCollection + "/" + now.toDateString().replace(/ /g, '') + '_' + now.getTime();
+    const tempFilePath = '/tmp/' + outputFileName;
 
-    var connectionString ='mongoexport --host='+host+' --username='+username+' --password='+password+' --authenticationDatabase=admin --db='+dbName+' --collection=submissions --out=' + folderName;
-    if (username=='defaultauthdb'){
-        //dev has no auth
-        connectionString ='mongoexport --host='+host+' --db='+dbName+' --collection=submissions --out=' + folderName;
-    } else{
-        connectionString ='mongoexport --host='+host+' --username='+username+' --password='+password+' --authenticationDatabase=admin --db='+dbName+' --collection=submissions --out=' + folderName;
-    }
-
-    exec(connectionString, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
+    // clean bucket
+    s3bucket.listObjects({ Bucket: bucketName[0], Delimiter: '/', Prefix: folder }, function(err, data) {
+        if (err) {
+            console.error(`Error listing bucket objects`, err);
             return;
         }
-        let filePath = "/tmp/" + fileName;
+        const items = data.Contents;
+        console.log(`Found: ${items.length} bucket objects`);
 
-        fs.readFile(filePath, function(err, data) {
+        for (let i = 0; i < items.length; i++) {
+            const deleteParams = { Bucket: bucketName[0], Key: items[i].Key };
+            s3bucket.deleteObject(deleteParams, function(err, data) {
+                const fullFilePath = `${bucketName[0]}/${deleteParams.Key}`;
+                if (err) {
+                    console.error(`Delete error ${fullFilePath}`, err);
+                } else {
+                    console.log(`Deleted ${fullFilePath}`);
+                }
+            });
+        }
+    });
+
+    // build mongo command
+    let mongoExportCommand = 'mongoexport --host=' + mongoHost;
+    if (mongoUsername !== 'defaultauthdb') {
+        // username supplied so set username and password
+        mongoExportCommand += ' --username=' + mongoUsername + ' --password=' + mongoPassword + ' --authenticationDatabase=admin';
+    }
+    mongoExportCommand += ' --db=' + mongoDBName + ' --collection=' + mongoCollection + ' --out=' + tempFilePath;
+
+    // execute command
+    exec(mongoExportCommand, function(error, stdout, stderr) {
+        if (error) {
+            console.error('exec error:', error);
+            return;
+        }
+
+        fs.readFile(tempFilePath, function(err, data) {
+            if (err) {
+                console.error('ERROR', err);
+                return;
+            }
             s3bucket.createBucket(function() {
                 let params = {
-                    Key: fileName, 
+                    Key: outputFileName,
                     Body: data
                 };
                 s3bucket.upload(params, function(err, data) {
-                    fs.unlink(filePath, function(err) {
+                    fs.unlink(tempFilePath, function(err) {
                         if (err) {
-                            console.error(err);
+                            console.error('ERROR', err);
+                        } else {
+                            console.log(`Temp File Deleted ${tempFilePath}`);
                         }
-                        console.log('Temp File Delete');
                     });
                     if (err) {
-                        console.log('ERROR', err);
+                        console.error('ERROR', err);
                     } else {
-                        console.log('Successfully uploaded data');
-
+                        console.log(`Successfully uploaded data to ${s3Path}/${outputFileName}`);
                     }
                 });
             });
